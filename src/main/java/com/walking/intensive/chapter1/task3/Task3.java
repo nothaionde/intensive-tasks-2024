b0cbf2f484a5cafdf0f3e5e6d255df179925bea8
nothaionde <78284605+nothaionde@users.noreply.github.com>
2024-12-21 17:30:37 +0500

    Task 3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -24,13 +24,11 @@ package com.walking.intensive.chapter1.task3;
  * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
  */
 public class Task3 {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-    }
+	}
 
-    static boolean isLeap(int year) {
-        //Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
-    }
+	static boolean isLeap(int year) {
+		return year >= 0 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+	}
 }
