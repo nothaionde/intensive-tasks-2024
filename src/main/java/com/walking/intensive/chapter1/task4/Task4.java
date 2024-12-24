8e5cd5c770966c8fe978dbb913f4b0a8355da519
nothaionde <78284605+nothaionde@users.noreply.github.com>
2024-12-25 00:47:52 +0500

    Task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter1.task4;
 
+import java.util.Arrays;
+
 /**
  * Дано уравнение:
  *
@@ -23,6 +25,9 @@ package com.walking.intensive.chapter1.task4;
  * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
  */
 public class Task4 {
+
+    private static final int PRECISION = 1;
+
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
@@ -34,8 +39,47 @@ public class Task4 {
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0) {
+            return solveLinearEquation(b, c);
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant > 0) {
+            return solveWithTwoRoots(a, b, discriminant);
+        } else if (discriminant == 0) {
+            return solveWithOneRoot(a, b);
+        } else {
+            return "Количество решений: 0.";
+        }
+    }
+
+    private static String solveLinearEquation(double b, double c) {
+        if (b == 0) {
+            return (c == 0) ? "Бесконечное множество решений." : "Количество решений: 0.";
+        }
+        double root = normalizeRoot(-c / b);
+        return String.format("Количество решений: 1. Корень: %s", formatWithPrecision(root));
+    }
+
+    private static String solveWithTwoRoots(double a, double b, double discriminant) {
+        double rootOne = normalizeRoot((-b - Math.sqrt(discriminant)) / (2 * a));
+        double rootTwo = normalizeRoot((-b + Math.sqrt(discriminant)) / (2 * a));
+        double[] roots = {rootOne, rootTwo};
+        Arrays.sort(roots);
+        return String.format("Количество решений: 2. Корни: %s;%s",
+                formatWithPrecision(roots[0]), formatWithPrecision(roots[1]));
+    }
+
+    private static String solveWithOneRoot(double a, double b) {
+        double root = normalizeRoot(-b / (2 * a));
+        return String.format("Количество решений: 1. Корень: %s", formatWithPrecision(root));
+    }
+
+    private static double normalizeRoot(double root) {
+        return root == -0.0 ? 0.0 : root;
+    }
+
+    private static String formatWithPrecision(double value) {
+        return String.format("%." + PRECISION + "f", value);
     }
 }
